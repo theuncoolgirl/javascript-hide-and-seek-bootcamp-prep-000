89b243e8053725f15acdcb99eba26bf49a7b7ab6
Erin Shields <elshields92@gmail.com>
2020-04-22 14:30:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const currentRanks = document.getElementsByClassName('ranked-list')
+  const firstList = currentRanks[0]
+  const secondList = currentRanks[1]
+  var firstChildren = firstList.children
+  var secondChildren = secondList.children
+
+  for (let i = 0; firstChildren.length > i; i++) {
+
+  firstChildren[i].innerHTML = parseInt(firstChildren[i].innerHTML) + n
+  }
+  for (let i = 0; secondChildren.length > i; i++) {
+
+  secondChildren[i].innerHTML = parseInt(secondChildren[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var element = document.getElementById('grand-node').querySelectorAll('div')
+  return element[element.length - 1]
+}
