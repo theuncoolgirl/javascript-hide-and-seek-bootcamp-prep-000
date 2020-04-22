c65ee18fa8321e4a4539101e2ae3a38848e3acb9
Erin Shields <elshields92@gmail.com>
2020-04-22 13:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function getFirstSelector(selector) {
 }
 
 function netstedTarget() {
-  var nested = document.getElementById(nested)
-  return document.querySelector('nested.target')
+  const nested = document.getElementById('nested')
+  return document.querySelector(nested.target)
 }
