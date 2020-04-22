f05bdc18075554d3fab7eb025ace2d8829634dfd
Erin Shields <elshields92@gmail.com>
2020-04-22 14:24:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var element = document.getElementById('grand-node')
-  while (element.children) {
-    element = element.children[0]
-  }
-  return element.innerHTML
+//  var element = document.getElementById('grand-node')
+//  while (element.children) {
+//    element = element.children[0]
+//  }
+  return document.querySelector('#grand-node div div div div')
 }
