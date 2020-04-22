7b12aaf21af189ecadd9556d689109f3a218e18c
Erin Shields <elshields92@gmail.com>
2020-04-22 13:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function netstedTarget() {
+function nestedTarget() {
   const nested = document.getElementById('nested')
   return document.querySelector(nested.target)
 }
